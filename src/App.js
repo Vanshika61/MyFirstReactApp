fd10154e2e74e2e47b9b406f149cc08a9e3ac0d3
Vanshika <agrawal10614@gmail.com>
2023-03-01 14:10:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm";
 import About from "./components/About";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState('light');     // wheather dark mode is enable or not
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode}/>
       <div className="container my-4">
         <TextForm heading="Enter the text to analyze below" />
       </div>
