16f0d89b3ef1c256991fe24ba9ff76329d925b42
Vanshika <agrawal10614@gmail.com>
2023-03-12 23:19:20 +0530

    add features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm";
-// import About from "./components/About";
+import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
 
@@ -26,7 +26,7 @@ function App() {
       setBtnText('Enable LightMode')
       document.body.style.backgroundColor = '#042743'
       showAlert('Dark mode has been enabled', 'success');
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
       
       // setInterval(() => {
       //   document.title = 'TextUtils is amazing';
@@ -40,7 +40,7 @@ function App() {
       setBtnText('Enable DarkMode')
       document.body.style.backgroundColor = 'white'
       showAlert('Light mode has been enabled', 'success');
-      document.title = 'TextUtils - Light Mode'
+      // document.title = 'TextUtils - Light Mode'
     }
   }
   return (
@@ -50,9 +50,9 @@ function App() {
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} btnText={btnText} />
       <Alert alert={alert} />
       <div className="container my-4">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+        <TextForm showAlert={showAlert} heading="Try TextUtils - Sentence counter, Word counter, Character counter, Remove extra spaces" mode={mode} />
       </div>
-      {/* <About/> */}
+      <About mode={mode} />
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -46,7 +46,7 @@ export default function TextForm(props) {
   return (
     <>
     <div className='container' style={{color : props.mode==='dark'?'white':'black'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-4'>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode==='dark'?'#042743':'white', color : props.mode==='dark'?'white':'black'}} id="myText" rows="8"></textarea>
       </div>
@@ -59,10 +59,10 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3" style={{color : props.mode==='dark'?'white':'black'}}>
         <h2>Your text Summary</h2>
-        <p> {text.split(".").filter((element)=>{return element.length!==0}).length} Sentences</p>
-        <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} Words</p>
+        <p> {text.split(".").filter((element)=>{return element.length !== 0 }).length} Sentences</p>
+        <p> {text.split(" ").filter((element)=>{return element.length !== 0 }).length} Words</p>
         <p> {text.length} Characters</p>
-        <p> {0.008 * text.split(" ").length} minutes to read </p>
+        <p> {0.008 * text.split(" ").filter((element)=>{return element.length !== 0 }).length} minutes to read </p>
         <h3>Perview</h3>
         <p>{text.length>0?text:'Enter something in the above textbox to preview it here.'}</p>
     </div>
